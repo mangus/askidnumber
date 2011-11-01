78c425d2b69aeaaa5c44e0ff67e12b76046dde4c
Mart Mangus <mart@mangus.ee>
2011-11-01 14:38:39 +0300

    "Require" -> "Ask" (2)

---
+++ lang/en/auth_askidnumber.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['auth_askidnumberdescription'] = 'This plugin asks users to insert their id-number during login if it is not set in user profile';
+$string['pluginname'] = 'ask ID-number';
+
+$string['pleaseinsertyouridnumber'] = 'Pleas insert your ID-number';
+
