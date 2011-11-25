91156ce5f3619237311f9720a347bddd65ee395b
eneli <eneli@eitsa.ee>
2011-11-25 11:49:11 +0200

    Ma selle ebakorrektse jätsin ära. Kas neil on võimalik ebakorrektset panna üldse?

--- lang/et/auth_askidnumber.php
+++ lang/et/auth_askidnumber.php
@@ -1,15 +1,15 @@
 <?php
 
-$string['auth_askidnumberdescription'] = 'See moodul küsib kasutajalt isikukoodi sisestamist pärast sisselogimist, kui isikukood ei ole veel sisestatud või on ebakorrektne';
+$string['auth_askidnumberdescription'] = 'See moodul küsib pärast sisselogimist isikukoodi neilt kasutajalt, kes pole seda veel sisestanud';
 $string['pluginname'] = 'Küsi isikukoodi pärast sisselogimist';
 
 $string['pleaseinsertyouridnumber'] = 'Palun sisesta oma isikukood';
-$string['youridnumberwhy'] = 'Isikukood Moodles on vajalik, et kasutajatel oleks võimalik ID-kaardiga sisse logida.<br />Kõik Moodle kasutajad peaksid sisestama oma isikukoodi.';
+$string['youridnumberwhy'] = 'Isikukood Moodles on vajalik, et kasutajatel oleks võimalik ID-kaardiga sisse logida.<br />Kõik Moodle kasutajad peavad sisestama oma isikukoodi.';
 $string['youridnumber'] = 'Sinu isikukood';
 
-$string['err_whyempty'] = 'Palun sisesta all olevasse lahtrisse oma isikukood';
+$string['err_whyempty'] = 'Palun sisesta allolevasse lahtrisse oma isikukood';
 $string['err_wronglenght'] = 'Isikukood peab olema täpselt 11 numbrit pikk';
 $string['err_notnumber'] = 'Isikukood tohib sisaldada ainult numbreid';
-$string['err_notunique'] = 'Sisestatud isikukoodiga kasutaja on juba Moodles registreeritud. Palun kontakteeru Moodle kasutajatoega.';
+$string['err_notunique'] = 'Sisestatud isikukoodiga kasutaja on Moodles juba olemas. Palun võta ühendust Moodle kasutajatoega.';
 $string['err_incorrectid'] = 'Sisestatud isikukood ei ole korrektne Eesti isikukood';
 
