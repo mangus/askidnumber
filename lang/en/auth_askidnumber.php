aeea555fd68abe4fb37779455a884f30c2f87818
Mart Mangus <mart@mangus.ee>
2011-12-13 15:33:55 +0300

    Support e-mail address

--- lang/en/auth_askidnumber.php
+++ lang/en/auth_askidnumber.php
@@ -10,6 +10,6 @@ $string['youridnumber'] = 'Your ID-number';
 $string['err_whyempty'] = 'Please insert your ID-number';
 $string['err_wronglenght'] = 'The ID-number should be exactly 11 numbers long';
 $string['err_notnumber'] = 'The ID-number should contain only numbers';
-$string['err_notunique'] = 'Inserted ID-number already exists in Moodle database. Please contact Moodle helpdesk.';
+$string['err_notunique'] = 'Inserted ID-number already exists in Moodle database. Please contact Moodle helpdesk (<a href="mailto:moodle@eitsa.ee">moodle@eitsa.ee</a>).';
 $string['err_incorrectid'] = 'Inserted ID-number is not a correct Estonian ID-number';
 
--- lang/et/auth_askidnumber.php
+++ lang/et/auth_askidnumber.php
@@ -10,6 +10,6 @@ $string['youridnumber'] = 'Sinu isikukood';
 $string['err_whyempty'] = 'Palun sisesta allolevasse lahtrisse oma isikukood';
 $string['err_wronglenght'] = 'Isikukood peab olema täpselt 11 numbrit pikk';
 $string['err_notnumber'] = 'Isikukood tohib sisaldada ainult numbreid';
-$string['err_notunique'] = 'Sisestatud isikukoodiga kasutaja on Moodles juba olemas. Palun võta ühendust Moodle kasutajatoega.';
+$string['err_notunique'] = 'Sisestatud isikukoodiga kasutaja on Moodles juba olemas. Palun võta ühendust Moodle kasutajatoega (<a href="mailto:moodle@eitsa.ee">moodle@eitsa.ee</a>).';
 $string['err_incorrectid'] = 'Sisestatud isikukood ei ole korrektne Eesti isikukood';
 
