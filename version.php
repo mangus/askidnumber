4f3f169660513bbdb31ff24a4b1995a2ac7751d1
Mart Mangus <mart@mangus.ee>
2012-10-23 00:09:54 +0300

    Correct versioning info

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Version information
+ *
+ * @author Mart Mangus
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2012102300;
+$plugin->component = 'auth_askidnumber';
